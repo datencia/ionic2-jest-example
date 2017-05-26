5d6a0aa7af3920ff1622db0378befa31d2a0b5d8
David Atencia <david.atencia@gmail.com>
2017-05-26 23:38:17 +0200

    add tests for the home page

---
+++ src/pages/home/home.spec.ts
@@ -0,0 +1,48 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { HomePage } from './home';
+
+describe('HomePage', () => {
+
+  let fixture: ComponentFixture<HomePage>;
+  let instance: HomePage;
+
+  const navControllerStub = {};
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ],
+      declarations: [
+        HomePage
+      ],
+      providers: [
+        {provide: NavController, useValue: navControllerStub}
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePage);
+    instance = fixture.debugElement.componentInstance;
+  });
+
+  it('should create the home page', () => {
+    expect(instance).toBeTruthy();
+  });
+
+  it('should show a title', () => {
+    const h2: HTMLHeadingElement = fixture.debugElement.query(By.css('ion-title')).nativeElement;
+    expect(h2.textContent).toContain('Home');
+  });
+
+  it('should show a greeting', () => {
+    const h2: HTMLHeadingElement = fixture.debugElement.query(By.css('h2')).nativeElement;
+    expect(h2.textContent).toContain('Welcome to Ionic!');
+  });
+
+});
