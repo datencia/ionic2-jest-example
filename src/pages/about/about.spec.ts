4c1551839c9a219d50376e768c0b7053aaeead32
David Atencia <david.atencia@gmail.com>
2018-05-10 20:30:23 +0200

    test(about): add tests for an AlertController

--- src/pages/about/about.spec.ts
+++ src/pages/about/about.spec.ts
@@ -1,7 +1,7 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestBed, ComponentFixture, inject } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
+import { Alert, AlertController } from 'ionic-angular';
 
 import { AboutPage } from './about';
 
@@ -10,7 +10,14 @@ describe('AboutPage', () => {
   let fixture: ComponentFixture<AboutPage>;
   let instance: AboutPage;
 
-  const navControllerStub = {};
+  let alert: Alert = <Alert>{
+    present: (): Promise<any> => Promise.resolve(),
+    dismiss: (): Promise<any> => Promise.resolve()
+  };
+
+  const alertCtrlStub = {
+    create: (options: any): Alert => alert
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -21,7 +28,7 @@ describe('AboutPage', () => {
         AboutPage
       ],
       providers: [
-        {provide: NavController, useValue: navControllerStub}
+        {provide: AlertController, useValue: alertCtrlStub}
       ]
     }).compileComponents();
   });
@@ -39,5 +46,21 @@ describe('AboutPage', () => {
     const h2: HTMLHeadingElement = fixture.debugElement.query(By.css('ion-title')).nativeElement;
     expect(h2.textContent).toContain('About');
   });
+
+  it('should show a button to open an alert', () => {
+    const button: HTMLButtonElement = fixture.debugElement.query(By.css('button')).nativeElement;
+    expect(button.textContent).toBe('Show Alert');
+  });
+
+  it('should show an alert dialog when the alert button is clicked', inject([AlertController], (alertCtrl: AlertController) => {
+    jest.spyOn(alertCtrl, 'create');
+    jest.spyOn(alert, 'present');
+
+    const button: DebugElement = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', null);
+    
+    expect(alertCtrl.create).toHaveBeenCalledWith({buttons: ['Dismiss'], subTitle: '10% of battery remaining', title: 'Low battery'});
+    expect(alert.present).toHaveBeenCalled();
+  }));
 
 });
